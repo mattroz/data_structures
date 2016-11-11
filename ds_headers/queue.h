0b6f543068e7348dd920df94f0753ff545ac423e
mattroz <matveyrozanov@gmail.com>
2016-11-11 10:02:59 +0300

    Deallocation added

--- ds_headers/queue.h
+++ ds_headers/queue.h
@@ -15,10 +15,19 @@ int enqueue(Node**, int);
 int dequeue(Node**, int*);
 int empty(Node **);
 
+/*	link head and tail to each other	*/
+int initialize_queue(Node *_head, Node *_tail)
+{
+	_tail->next = NULL;
+	_head->next = _tail;
+	printf("initialization\n");
+
+	return 1;
+}
+
 
 int enqueue(Node **_tail, int _value)
 {
-	
 	Node *new_node = malloc(sizeof Node);
 	new_node->value = _value;
 	new_node->next = NULL;
@@ -32,10 +41,11 @@ int enqueue(Node **_tail, int _value)
 
 int dequeue(Node **_head, int *dest)
 {
-	Node **temp = *_head;
+	Node **temp = &_head;
 	*dest = (*_head)->value;
 	*_head = (*_head)->next;
-	free(*temp);
+	free(*temp);	//free memory from the previous head
+	free(temp);		//free memory from the current temp pointer
 	printf("dequeue %d\n", *dest);
 
 	return 1;
