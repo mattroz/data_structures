6ed2b9b5c441359eab9484d2ad80a9d57e96d301
mattroz <matveyrozanov@gmail.com>
2016-11-11 10:24:19 +0300

    Fixes while debugging

--- ds_headers/queue.h
+++ ds_headers/queue.h
@@ -2,24 +2,25 @@
 #define QUEUE_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /*	Queue element definition	*/
 typedef struct Node
 {
 	int value;
-	Node *next;
-}Node;
+	struct Node *next;
+}Node_t;
 
 /*	PROTOTYPES	*/
-int initialize_queue(Node*, Node*);
-int enqueue(Node**, int);
-int dequeue(Node**, int*);
-int is_empty(Node **);
+int initialize_queue(Node_t*, Node_t*);
+int enqueue(Node_t**, int);
+int dequeue(Node_t**, int*);
+int is_empty(Node_t *);
 
 /*		IMPLEMENTATION	*/
 
 /*	link head and tail to each other	*/
-int initialize_queue(Node *_head, Node *_tail)
+int initialize_queue(Node_t *_head, Node_t *_tail)
 {
 	_tail->next = NULL;
 	_head->next = _tail;
@@ -29,9 +30,9 @@ int initialize_queue(Node *_head, Node *_tail)
 }
 
 
-int enqueue(Node **_tail, int _value)
+int enqueue(Node_t **_tail, int _value)
 {
-	Node *new_node = malloc(sizeof Node);
+	Node_t *new_node = malloc(sizeof Node_t);
 	new_node->value = _value;
 	new_node->next = NULL;
 	(*_tail)->next = new_node;
@@ -42,7 +43,7 @@ int enqueue(Node **_tail, int _value)
 }
 
 
-int dequeue(Node **_head, int *dest)
+int dequeue(Node_t **_head, int *dest)
 {
 	/*	check if head == tail	*/
 	if(is_empty((*_head)) != 0)
@@ -51,20 +52,20 @@ int dequeue(Node **_head, int *dest)
 		return 1;
 	}
 	
-	Node **temp = &_head;
+	Node_t *temp = &_head;
 	*dest = (*_head)->value;
 	*_head = (*_head)->next;
-	free(*temp);	//free memory from the previous head
+	//free(*temp);	//free memory from the previous head
 	free(temp);		//free memory from the current temp pointer
 	printf("dequeue %d\n", *dest);
 
 	return 1;
 }
 
-int is_empty(Node *_head)
+int is_empty(Node_t *_head)
 {
 	/*  in other words: check if head == tail   */
-	return (((*_head)->next == NULL) : 1 ? 0);
+	return ((head->next == NULL) ? 1 : 0);
 }
 
 
